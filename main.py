d826cc93550ad532cac9f86d834310526303fa12
0-Rafael <andrerafaelduval@gmail.com>
2026-01-21 22:16:28 -0300

    att 3.0 sistema de pontos

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import json
 class Score():
     def __init__(self, caminho) -> None:
         self.caminho = caminho
-        if not os.path.exists(caminho):
+        if os.path.exists(caminho) and os.path.getsize(caminho)<1:
             self.conteudo= []
+        elif not os.path.exists(caminho):
+            self.conteudo = []
         else:
             self.conteudo = json.load(open(caminho, "r"))
+
+
     def salva_user(self, lista):
         file = open(self.caminho, "w")
         json.dump(lista, file, indent=4)
+        file.close()
+
+
     def existe_usuario(self, nome):
         file = self.conteudo
         for n in file:
@@ -17,28 +24,25 @@ class Score():
                 return True
             else:
                 return False
+            
+
     def add_user(self, usuario: dict):
-        # usuarios = self.conteudo
-        # if Score.existe_usuario(self, nome= usuario["nome"]):
-        #     for n in usuarios:
-        #         if n["nome"]==usuario["nome"]:
-        #             n["score"] = usuario["score"]
-        # else:
-        #     usuarios.append(usuario)
-        # Score.salva_user(self, lista=usuarios)
         usuario_limpo = {
             "nome": str(usuario["nome"]),
             "score": int(usuario["score"])
         }
 
         for u in self.conteudo:
-            if u["nome"] == usuario_limpo["nome"] and u["score"]< usuario_limpo["score"]:
-                u["score"] = usuario_limpo["score"]
-                Score.salva_user(self, lista=self.conteudo)
+            if u["nome"] == usuario_limpo["nome"]:
+                if u["score"]< usuario_limpo["score"]:
+                    u["score"] = usuario_limpo["score"]
+                    self.salva_user(self.conteudo)
+                    return
+                self.salva_user(self.conteudo)
                 return
 
         self.conteudo.append(usuario_limpo)
-        Score.salva_user(self, lista=self.conteudo)
+        self.salva_user(self.conteudo)
 
 class Home():
     def __init__(self) -> None:
