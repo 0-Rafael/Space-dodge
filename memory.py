adfc1a6ea75361bc13e460a1b773082d29b551c2
0-Rafael <andrerafaelduval@gmail.com>
2026-01-28 10:12:19 -0300

    att

--- memory.py
+++ memory.py
@@ -25,50 +25,42 @@ que jogaram o jogo com suas respectivas pontuações.
 
 import turtle
 import random
-
+from time import sleep
 from freegames import path
 
 carro = path('car.gif')
 pecas = list(range(32)) * 2
 estado = {'marca': None}
 escondido = [True] * 64
-
+toques = 0
+game = False
+pecas_4_4 = list(range(8)) * 2
+escondido_4_4 = [True] * 16
 
 def quadrado(x, y):
     """Desenha um quadrado branco com contorno preto em (x, y)."""
     #TODO rescrever de forma que desenhe um quadrado a partir do ponto (x,y)
     # com 50 pixels de lado
-    # turtle.up()
-    # turtle.goto(-210, -210)
-    # turtle.down()
-    # turtle.color('black', 'white')
-    # turtle.begin_fill()
-    # for _ in range(4):
-    #     turtle.forward(420)
-    #     turtle.left(90)
-    # turtle.end_fill()
-    # turtle.left(90)
-    turtle.color("black", "white")
     turtle.up()
-    init = -210
-    turtle.goto(-210, -210)
+    turtle.goto(x, y)
     turtle.down()
-    turtle.left(90)
-    for n in range(8):
-        for n in range(8):
-            turtle.begin_fill()
-            for n in range(4):
-                turtle.fd(50)
-                turtle.left(90)
-            turtle.end_fill()
-            turtle.left(90)
-            turtle.fd(50)
-        turtle.up()
-        init+=50
-        turtle.goto(-210, init)
-        turtle.down()
+    turtle.color('black', 'white')
+    turtle.begin_fill()
+    for _ in range(4):
+        turtle.forward(50)
+        turtle.left(90)
+    turtle.end_fill()
 
-
+def quadrado_4(x,y):
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.down()
+    turtle.color('black', 'white')
+    turtle.begin_fill()
+    for _ in range(4):
+        turtle.forward(50)
+        turtle.left(90)
+    turtle.end_fill()
 
 
 
@@ -86,7 +78,9 @@ def toque(x, y):
     """Atualiza a marcação e as peças escondidas com base no clique."""
     posicao = indice(x, y)
     marca = estado['marca']
-
+    global toques
+    toques+=1
+    print(toques)
     if marca is None or marca == posicao or pecas[marca] != pecas[posicao]:
         estado['marca'] = posicao
     else:
@@ -94,32 +88,43 @@ def toque(x, y):
         escondido[marca] = False
         estado['marca'] = None
 
+def init():
+    turtle.goto(0,0)
+    mensangem = turtle.write("Bem vindo ao\njogo da memoria", font=('Arial', 18, 'normal'), align="Center")
+    username = turtle.numinput("Username", "Digite seu username: ")
 
+    game= True
+    return
 def desenhar():
     """Desenha a imagem e as peças."""
+    global game
+    init()
+    if game:
+        turtle.clear()
+        turtle.goto(0, 0)
+        turtle.shape(carro)
+        turtle.stamp()
+        global toques
+        for contador in range(64):
+            if escondido[contador]:
+                x, y = coordenadas(contador)
+                quadrado(x, y)
+        if toques>=3:
+            return game_over()
+        marca = estado['marca']
+        if marca is not None and escondido[marca]:
+            x, y = coordenadas(marca)
+            turtle.up()
+            turtle.goto(x + 2, y)
+            turtle.color('black')
+            turtle.write(pecas[marca], font=('Arial', 30, 'normal'))
+        turtle.update()
+        turtle.ontimer(desenhar, 100)
+
+def game_over():
     turtle.clear()
-    turtle.goto(0, 0)
-    turtle.shape(carro)
-    turtle.stamp()
-
-    for contador in range(64):
-        if escondido[contador]:
-            x, y = coordenadas(contador)
-            quadrado(x, y)
-
-    marca = estado['marca']
-
-    if marca is not None and escondido[marca]:
-        x, y = coordenadas(marca)
-        turtle.up()
-        turtle.goto(x + 2, y)
-        turtle.color('black')
-        turtle.write(pecas[marca], font=('Arial', 30, 'normal'))
-
-    turtle.update()
-    turtle.ontimer(desenhar, 100)
-
-
+    turtle.goto(-60,0)
+    turtle.write("Acabou", font=('Arial', 30, 'normal'))
 random.shuffle(pecas)
 
 turtle.setup(420, 420, 370, 0)
